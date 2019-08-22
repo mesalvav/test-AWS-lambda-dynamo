185baccfe8a506c988860b5e47f28f392c2a491a
mario salvatierra <msalv006@fiu.edu>
2019-08-22 11:22:01 -0400

    get item from DB using DocumentClient

---
+++ README.md
@@ -0,0 +1,7 @@
+#AWS lambda and DynamoDB
+## The final goal is to learn how to build  an API in a microservice style.
+## This can be achieved in AWS by using Lambda functions and DynamoDB
+
+by Following a tutorial at:
+https://www.youtube.com/watch?v=ijyeE-pXFk0
+
--- getUserData/index.js
+++ getUserData/index.js
@@ -4,19 +4,24 @@ AWS.config.update({region: "us-east-2"});
 
 exports.handler = function (event, context, callback) {
     const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
+    //unmarshall dynamoDB with document client
+  const documentClient  = new AWS.DynamoDB.DocumentClient({region: "us-east-2"});
+  var params = {
+    TableName: "Users",
+    Key: { id:  "12345"}
+   };
+  // before document client used: params as:
+  /*
     var params = {
       TableName: "Users",
       Key: {
        id: {
          S: "12345"
-        }
-       
-      }
-      
-     };
+        }   } };
+        */
 
-     ddb.getItem(params, (err, data) => {
+// before document client used:  ddb.getItem(params, (err, data) => {...
+     documentClient.get(params, (err, data) => {
       if (err) console.log(err, err.stack); // an error occurred
       else     console.log(data);           // successful response
       
