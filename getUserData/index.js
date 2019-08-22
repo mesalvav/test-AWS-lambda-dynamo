701d09884023d2c0d504b7a891089930190c6401
mario salvatierra <msalv006@fiu.edu>
2019-08-22 11:41:56 -0400

    refactor lambda function to async-await

--- getUserData/index.js
+++ getUserData/index.js
@@ -2,7 +2,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({region: "us-east-2"});
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context, callback) => {
     const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     //unmarshall dynamoDB with document client
   const documentClient  = new AWS.DynamoDB.DocumentClient({region: "us-east-2"});
@@ -20,10 +20,12 @@ exports.handler = function (event, context, callback) {
         }   } };
         */
 
-// before document client used:  ddb.getItem(params, (err, data) => {...
-     documentClient.get(params, (err, data) => {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-      
-    });
+// before document client it was used like:  ddb.getItem(params, (err, data) => {...
+ 
+    try {
+      const data = await documentClient.get(params).promise();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
 }
