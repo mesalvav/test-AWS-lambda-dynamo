38238c6c8d5db8b4444b0721a4701949ec1c6926
mario salvatierra <msalv006@fiu.edu>
2019-08-22 10:41:49 -0400

    get item function made arrow function

--- getUserData/index.js
+++ getUserData/index.js
@@ -1,22 +1,22 @@
 'use strict'
 const AWS = require('aws-sdk');
-
 AWS.config.update({region: "us-east-2"});
 
 exports.handler = function (event, context, callback) {
     const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
     var params = {
+      TableName: "Users",
       Key: {
        id: {
          S: "12345"
         }
        
-      }, 
-      TableName: "Users"
+      }
+      
      };
 
-     ddb.getItem(params, function(err, data) {
+     ddb.getItem(params, (err, data) => {
       if (err) console.log(err, err.stack); // an error occurred
       else     console.log(data);           // successful response
       
