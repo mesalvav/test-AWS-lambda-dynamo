9549d700aa152913ee699aade4d679ba3ef7b12e
mario salvatierra <msalv006@fiu.edu>
2019-08-22 09:43:39 -0400

    first test good

--- getUserData/index.js
+++ getUserData/index.js
@@ -1,6 +1,24 @@
 'use strict'
 const AWS = require('aws-sdk');
 
+AWS.config.update({region: "us-east-2"});
+
 exports.handler = function (event, context, callback) {
-    
+    const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+    var params = {
+      Key: {
+       id: {
+         S: "12345"
+        }
+       
+      }, 
+      TableName: "Users"
+     };
+
+     ddb.getItem(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+      
+    });
 }
